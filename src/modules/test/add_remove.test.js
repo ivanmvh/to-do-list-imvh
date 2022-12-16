30a9d40f814d31e6b88dc3966590536adc3fe271
mirnamp <ampueromirna8@gmail.com>
2022-12-16 11:20:40 -0500

    add lintersn

--- src/modules/test/add_remove.test.js
+++ src/modules/test/add_remove.test.js
@@ -2,20 +2,18 @@
  * @jest-environment jsdom
  */
 
-import { todosData } from '../Todos_Data';
+import { todosData } from '../Todos_Data.js';
 
 describe('add and remove', () => {
   test('addTodo method test', () => {
-    todosData.addTodo("description1");
-    todosData.addTodo("description2");
+    todosData.addTodo('description1');
+    todosData.addTodo('description2');
     expect(JSON.parse(localStorage.getItem('todos'))[0].description).toBe('description1');
     expect(JSON.parse(localStorage.getItem('todos'))[1].description).toBe('description2');
   });
 
   test('remove method test', () => {
-    console.log(1,localStorage.getItem('todos'));
     todosData.removeTodo(1);
-    console.log(2,localStorage.getItem('todos'));
     expect(JSON.parse(localStorage.getItem('todos')).length).toBe(1);
   });
 });
